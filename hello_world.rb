fd6b4f803609d45f0dd046428c6e1e59127ff9bd
Edward Helman <eddhelman@gmail.com>
2016-04-15 20:08:00 -0400

    takes input and outputs whats put in

--- hello_world.rb
+++ hello_world.rb
@@ -4,3 +4,12 @@ require 'sinatra'
 get '/' do
 "Hello World"
 end
+
+get '/isbn_input' do
+	erb :isbn_web, :locals => {:answer => ""}
+end
+
+post '/isbn_input' do
+	isbn_input = params[:isbn_input]
+	erb :isbn_web, :locals => {:answer => isbn_input}	
+end
